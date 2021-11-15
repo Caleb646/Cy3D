15c89f8e38b800d71620cfac9fbbaeb9b5d4301f
Caleb <calebthomas646@yahoo.com>
2021-11-14 22:05:42 -0600

    first commit

---
+++ AddSubMods.bat
@@ -0,0 +1,3 @@
+git submodule add https://github.com/Caleb646/Logi.git deps/include/Logi
+git submodule add https://github.com/Caleb646/M3D.git deps/include/M3D
+git submodule add https://github.com/Caleb646/Tent.git deps/include/Tent
