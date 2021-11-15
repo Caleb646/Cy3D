0d0b633977e3f782a1bfe68fffe9624571498820
Caleb <calebthomas646@yahoo.com>
2021-11-15 14:49:49 -0600

    updated project settings.

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,33 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-//TODO fix linking error
-//#include <Tent/Tent.h>
-//#include <M3D/M3D.h>
-//#include <Logi/Logi.h>
-
-
 #include <iostream>
 
+//check that libs are linked.
+#include <Logi/Logi.h>
+#include <M3D/M3D.h>
+#include <Tent/Tent.h>
+
+
 int main() {
-    glfwInit();
+    //glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    //GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    //uint32_t extensionCount = 0;
+    //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
+    //std::cout << extensionCount << " extensions supported\n";
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-    }
+    //while (!glfwWindowShouldClose(window))
+    //{
+    //    glfwPollEvents();
+    //}
 
-    glfwDestroyWindow(window);
+    //glfwDestroyWindow(window);
 
-    glfwTerminate();
+    //glfwTerminate();
 
     return 0;
 }
